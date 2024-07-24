06344d651c160a5aae336378a96566dd2cd509b5
ArtyrGetman <artyrgetman@gmail.com>
2024-07-24 19:33:53 +0300

    +11

--- src/Runner.java
+++ src/Runner.java
@@ -18,7 +18,7 @@ public class Runner {
         String errorTExt = ErrorsText.unknownErrorText;
         System.out.println(errorTExt);
 
-        System.out.println("1");
+        System.out.println("11");
 
 
     }
