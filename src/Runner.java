968de782288a8cb45939e1c46ce784bfd3d3e17e
ArtyrGetman <artyrgetman@gmail.com>
2024-07-24 19:43:58 +0300

    resolve conflicts

--- src/Runner.java
+++ src/Runner.java
@@ -18,7 +18,7 @@ public class Runner {
         String errorTExt = ErrorsText.unknownErrorText;
         System.out.println(errorTExt);
 
-        System.out.println("25");
+        System.out.println("45");
 
 
     }
